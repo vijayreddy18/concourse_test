670e176afeee16b0791a867ded70ce69a8dc215b
vijayreddy99 <vijay31453@gmail.com>
2020-09-27 11:38:37 +0530

    folder upload

---
+++ dotnet_build/hello_world.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to dotnet");
+        }
+    }
+}
